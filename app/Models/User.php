a47c2e85c04693d76cc05ded1203f9d66b82e4ee
Harish Chauhan <harish282@gmail.com>
2025-06-28 12:16:40 +0530

    PSR12 format using Laravel Pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
     use HasApiTokens;
 
     /**
